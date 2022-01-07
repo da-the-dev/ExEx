1bed31f08e448f32b68cb1babbb3729763204d85
da-the-dev <leshiktk@me.com>
2022-01-07 15:09:19 +0300

    Prevented users from creating duplicate extensions

--- src/commands/createProfile.ts
+++ src/commands/createProfile.ts
@@ -5,15 +5,22 @@ import ProfileService from '../core/services/profileService'
 const cmd = {
     name: 'createProfile',
     foo: async ctx => {
-        const profile = await vscode.window.showInputBox({
+        const profileName = (await vscode.window.showInputBox({
             title: 'Enter the profile\'s name',
             placeHolder: 'Very cool profile XD',
             ignoreFocusOut: true
-        })
-        if (!profile) {
+        }))?.trim()
+
+        if (!profileName) {
             vscode.window.showErrorMessage('No profile name was defined!')
             return
         }
+
+        if (ProfileService.profile(profileName, ctx)) {
+            vscode.window.showErrorMessage('Profile with this name already exists!')
+            return
+        }
+
 
         const extensions = await ExtensionService.fetchExtensions()
         const selectedExtensions = await vscode.window.showQuickPick(extensions.map(e => e.name), {
@@ -30,14 +37,14 @@ const cmd = {
         const enabledExtensions = extensions.filter(e => selectedExtensions.includes(e.name))
         const disabledExtensions = extensions.filter(e => !selectedExtensions.includes(e.name))
 
-        await ProfileService.createProfile(profile, enabledExtensions, disabledExtensions, ctx);
+        await ProfileService.createProfile(profileName, enabledExtensions, disabledExtensions, ctx);
 
         (await vscode.window.showQuickPick(['Enable', 'Skip'], {
             title: "Enable this profile?",
             placeHolder: 'Confirm enablement'
-        })) === 'Enable' ? await ProfileService.enableProfiles([ProfileService.profile(profile, ctx)!], ctx) : null
+        })) === 'Enable' ? await ProfileService.enableProfiles([ProfileService.profile(profileName, ctx)!], ctx) : null
 
-        await vscode.window.showInformationMessage(`Succesfully created "${profile}"!`)
+        await vscode.window.showInformationMessage(`Succesfully created "${profileName}"!`)
     }
 } as Command
 export { cmd }
