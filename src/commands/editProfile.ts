5d8e8dd753964cd746cba08796b3800481a6206b
da-the-dev <leshiktk@me.com>
2022-01-07 12:05:24 +0300

    Revert "Multistep pick p1"
    Wrong branch again
    
    This reverts commit e7e686b95c716fbb1a5e09c601f6c25cca1d0ed3.

--- src/commands/editProfile.ts
+++ src/commands/editProfile.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode'
 import Command from "../core/interfaces/Command"
-import MultiStepPick from '../core/modules/mulitsteppick'
 import ExtensionService from '../core/services/extensionService'
 import ProfileService from '../core/services/profileService'
 import StorageService from '../core/services/storageService'
@@ -14,18 +13,18 @@ const cmd = {
             return
         }
 
-        const selectedProfileName = await vscode.window.showQuickPick(profiles.map(p => p.name), {
+        const selectedProfile = await vscode.window.showQuickPick(profiles.map(p => p.name), {
             title: 'Select a profile to edit',
             placeHolder: 'Find a profile by name'
         })
-        if (!selectedProfileName) {
+        if (!selectedProfile) {
             vscode.window.showErrorMessage('No profile was selected!')
             return
         }
 
-        const profile = profiles.find(p => p.name === selectedProfileName)
+        const profile = profiles.find(p => p.name === selectedProfile)
         if (!profile) {
-            vscode.window.showErrorMessage(`No profile with name ${selectedProfileName} was found!`)
+            vscode.window.showErrorMessage(`No profile with name ${selectedProfile} was found!`)
             return
         }
         const rawExtensions = await ExtensionService.fetchExtensions()
@@ -37,21 +36,12 @@ const cmd = {
                         picked: profile.enabledExtensions.find(x => x.name === e.name) ? true : false
                     } as vscode.QuickPickItem
                 })
-                .sort((a, b) => {               // Sort by name
-                    return a.label >= b.label ? 1 : -1
-                })
-                .sort((a, b) => {               // Sort by picked
-                    if (a.picked && !b.picked) return -1
-                    if (!a.picked && b.picked) return 1
-                    return 0
-                })
-
 
 
         const selectedExtensions = await vscode.window.showQuickPick(extensions, {
             canPickMany: true,
             title: 'Select extensions you want in a profile',
-            placeHolder: 'Find extensions by name',
+            placeHolder: 'Find extensions by name'
         })
 
         if (!selectedExtensions) {
@@ -59,19 +49,17 @@ const cmd = {
             return
         }
 
-        await ProfileService.deleteProfile(selectedProfileName, ctx)
+        await ProfileService.deleteProfile(selectedProfile, ctx)
         await ProfileService.createProfile(
-            selectedProfileName,
+            selectedProfile,
             rawExtensions.filter(e => selectedExtensions.find(ex => e.name === ex.label)),
             rawExtensions.filter(e => !selectedExtensions.find(ex => e.name === ex.label)),
             ctx
         )
 
-        // Auto-reload if edited profile is enabled
-        if (StorageService.getWorkspaceKey<string>('xx.enabledProfiles', ctx)?.includes(selectedProfileName))
-            await vscode.commands.executeCommand('workbench.action.reloadWindow')
-
-        await vscode.window.showInformationMessage(`Succesfully edited "${selectedProfileName}"!`)
+        await vscode.window.showInformationMessage(
+            `Succesfully edited "${selectedProfile}"! ${StorageService.getWorkspaceKey<string>('xx.enabledProfiles', ctx)?.includes(selectedProfile) ? 'Re-enable profile to apply the changes!' : ''}`
+        )
     }
 } as Command
 export { cmd }
