33cbd7355e2a8438bad40c6e256aa7e5cce2108b
da-the-dev <leshiktk@me.com>
2022-01-15 17:45:13 +0300

    Better extension search

--- src/core/services/extensionService.ts
+++ src/core/services/extensionService.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode'
-import { readdirSync, readFileSync } from 'fs'
+import { readdirSync, readFileSync, readSync } from 'fs'
 import { homedir } from 'os'
 import { slash } from '../modules/slash'
 import { createUUID } from '../modules/uuid'
@@ -10,41 +10,43 @@ export default class ExtensionService {
     constructor() { }
 
     /**
-     * Fetches all installed extensions from `.vscode/extensions` folder
+     * Fetches all valid and installed extensions from `.vscode/extensions` folder
      */
     static async fetchExtensions(): Promise<Extension[]> {
-        const baseDir = `${homedir()}${slash}.vscode${slash}extensions`
-        return readdirSync(baseDir)
-            .filter(e => e !== '.DS_Store' && e !== '.obsolete')     // Filter out .DS_Store and '.obsolete' thing (maybe it's important, idk, idc)
+        const baseDir = `${homedir()}${slash}.vscode${slash}extensions${slash}`
+        const obsolete = new Set(Object.keys(JSON.parse(readFileSync(`${baseDir}.obsolete`).toString())))
+        const dir = readdirSync(baseDir)
+        const found: Set<string> = new Set()
+        return dir
+            .filter(a => {
+                if (a === '.obsolete' || a === '.DS_Store' || obsolete.has(a))
+                    return false
+
+                const aSplit = a.split('-').slice(0, -1).join('-')
+                if (!found.has(aSplit)) {
+                    const reps = dir.filter(b => b.split('-').slice(0, -1).join('-') == aSplit)
+                    const str = [a, ...reps].sort((x, y) => {
+                        const xVer = x.split('-').pop()!
+                        const yVer = y.split('-').pop()!
+                        if (xVer < yVer) return 1
+                        if (xVer === yVer) return 0
+                        return -1
+                    })[0]
+                    found.add(str.split('-').slice(0, -1).join('-'))
+                    return true
+                }
+                return false
+            })
             .map(e => {                             // Map extensions from a folder
-                const json = JSON.parse(readFileSync(`${baseDir}${slash}${e}${slash}package.json`).toString())
+                const json = JSON.parse(readFileSync(`${baseDir}${e}${slash}package.json`).toString())
 
                 return {
                     id: e.split('-').slice(0, -1).join('-'),
-                    name: /(%.*%)/gi.test(json.displayName) ? JSON.parse(readFileSync(`${baseDir}${slash}${e}${slash}package.nls.json`).toString())[json.displayName.slice(1, -1)] : json.displayName,
+                    name: /(%.*%)/gi.test(json.displayName) ? JSON.parse(readFileSync(`${baseDir}${e}${slash}package.nls.json`).toString())[json.displayName.slice(1, -1)] : json.displayName,
                     uuid: json.__metadata?.id
                 } as Extension
             })
             .sort((a, b) => {                       // Sort in alphabetical order by name
-                return a.name > b.name ? 1 : -1
-            })
-    }
-
-    /**
-     * Fetches all installed using VSCode API
-     * @deprecated Requires rethinking. VSCode API hides disabled extensions
-     */
-    static fetchExtensionsAPI(ctx: vscode.ExtensionContext) {
-        return vscode.extensions.all
-            .filter(e => !/.*(?:\\\\|\/)resources(?:\\\\|\/)app(?:\\\\|\/)extensions(?:\\\\|\/).*/i.test(e.extensionPath))
-            .map(e =>
-                new Extension(
-                    e.packageJSON.displayName,
-                    e.id.toLowerCase(),
-                    e.packageJSON.uuid,
-                )
-            )
-            .sort((a, b) => {
                 return a.name > b.name ? 1 : -1
             })
     }
